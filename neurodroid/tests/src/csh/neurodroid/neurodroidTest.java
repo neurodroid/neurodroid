8f3e79aa6fd3033da331dcdf0aedae7bc5bc5d05
Christoph Schmidt-Hieber <christsc@gmx.de>
2011-06-24 23:47:32 +0100

    Set up minimal test suit.

--- neurodroid/tests/src/csh/neurodroid/neurodroidTest.java
+++ neurodroid/tests/src/csh/neurodroid/neurodroidTest.java
@@ -2,6 +2,8 @@ package csh.neurodroid;
 
 import android.test.ActivityInstrumentationTestCase;
 
+import csh.neurodroid.NeuroDroid;
+
 /**
  * This is a simple framework for a test of an Application.  See
  * {@link android.test.ApplicationTestCase ApplicationTestCase} for more information on
@@ -9,13 +11,20 @@ import android.test.ActivityInstrumentationTestCase;
  * <p/>
  * To run this test, you can type:
  * adb shell am instrument -w \
- * -e class csh.neurodroid.neurodroidTest \
+ * -e class csh.neurodroid.NeuroDroidTest \
  * csh.neurodroid.tests/android.test.InstrumentationTestRunner
  */
-public class neurodroidTest extends ActivityInstrumentationTestCase<neurodroid> {
+public class NeuroDroidTest extends ActivityInstrumentationTestCase<NeuroDroid> {
 
-    public neurodroidTest() {
-        super("csh.neurodroid", neurodroid.class);
+    public NeuroDroidTest() {
+        super("csh.neurodroid.NeuroDroid", NeuroDroid.class);
     }
 
+    /**
+     * Verifies that the activity under test can be launched.
+     */
+    public void testActivityTestCaseSetUpProperly() {
+        assertNotNull("activity should be launched successfully", getActivity());
+    }
+    
 }

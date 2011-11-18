fe33ce9182a60449181ca069bc7b1eeee6915947
Christoph Schmidt-Hieber <christoph.schmidthieber@gmail>
2011-11-18 14:51:32 +0000

    Updated build for sdk >= 14

---
+++ nrn/test_agcc.cpp
@@ -0,0 +1,19 @@
+/* confdefs.h */
+#define PACKAGE_NAME "nrn"
+#define PACKAGE_TARNAME "nrn"
+#define PACKAGE_VERSION "7.2"
+#define PACKAGE_STRING "nrn 7.2"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_URL ""
+#define PACKAGE "nrn"
+#define VERSION "7.2"
+#define DISCRETE_EVENT_OBSERVER 1
+/* end confdefs.h.  */
+
+int
+main ()
+{
+
+  ;
+  return 0;
+}
